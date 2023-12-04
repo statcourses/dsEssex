6329d4cfc39d7c0dcbebb96bac208b5e69eea777
Osama <o.mahmoud@essex.ac.uk>
2023-12-04 14:56:07 +0000

    Auto stash before merge of "main" and "origin/main"

--- R/CI_log_OR.R
+++ R/CI_log_OR.R
@@ -6,7 +6,7 @@
 #' @param log_OR log odds ratios
 #' @param numerator the column whose data are the frequencies of the numerator
 #' @param denominator the column whose data are the frequencies of the denominator
-#' @param sig significance level
+#' @param sig significance level (i.e., 0.05 for 95% CI)
 #' @param upper logical, if TRUE (the default), return the upper limit of the confidence interval. Otherwise return its lower limit
 #'
 #' @return A vector of the confidence limit of the log odds ratios (either the upper or lower limit)
@@ -17,8 +17,8 @@
 CI_log_OR <- function(log_OR, numerator, denominator, sig = 0.05, upper = TRUE){
   SE = sqrt(1/numerator + 1/(sum(numerator) - numerator) + 1/denominator + 1/(sum(denominator) - denominator))
   if(upper){
-    return(log_OR + qnorm(sig/2) * SE)
+    return(log_OR + qnorm(1 - sig/2) * SE)
   } else {
-    return(log_OR - qnorm(sig/2) * SE)
+    return(log_OR - qnorm(1 - sig/2) * SE)
   }
 }
